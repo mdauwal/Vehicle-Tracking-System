dd7cbc5e6420d4ab2810b75b8230dc582a3cf589
Assad Isah <nottherealalanturing@gmail.com>
2024-09-13 21:12:30 +0100

    link routes

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { Outlet, Link } from "react-router-dom";
-import Beepboop from "../assets/Beepboop.png";
-import { BsBell } from "react-icons/bs";
-import { RiArrowDropDownLine } from "react-icons/ri";
+import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import Beepboop from '../assets/Beepboop.png';
+import { BsBell } from 'react-icons/bs';
+import { RiArrowDropDownLine } from 'react-icons/ri';
 
 const Layout = () => {
   return (
@@ -22,7 +22,7 @@ const Layout = () => {
 
               <div className="flex justify-center items-center">
                 <div className="ml-5">
-                  <img src={Beepboop} />
+                  <img src={Beepboop} alt="" />
                 </div>
                 <div class="  text-sm px-4 py-2">
                   <p>Admin's Name</p>
@@ -65,7 +65,7 @@ const Layout = () => {
             <ul class="flex flex-wrap flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <a
-                  href="#"
+                  href="/"
                   class="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500"
                   aria-current="page"
                 >
@@ -74,7 +74,7 @@ const Layout = () => {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="/activities"
                   class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Activities
@@ -82,7 +82,7 @@ const Layout = () => {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="/destination-tracking"
                   class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Destination Tracking
